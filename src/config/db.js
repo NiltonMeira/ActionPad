a245b3352505674c055d84562f6a848d953f746b
João Pedro <veigarsemr@gmail.com>
2024-04-05 13:46:04 -0300

    Branch created

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const sequelize = require('sequelize');
 //configurações da base de dados
 const database = new sequelize('db_projetox', 'master', '1234',
 {
-dialect: 'mssql', host:'localhost', port: 58775
+dialect: 'mssql', host:'localhost', port: 55537
 });
 database.sync();
 module.exports = database;
